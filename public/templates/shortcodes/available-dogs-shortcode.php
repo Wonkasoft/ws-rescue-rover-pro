bba148044c8982254f3bcb1ff170acb83f5785d0
MrLister <rlister@wonkasoft.com>
2024-06-06 18:00:31 -0700

    fixed page css

--- public/templates/shortcodes/available-dogs-shortcode.php
+++ public/templates/shortcodes/available-dogs-shortcode.php
@@ -24,34 +24,25 @@
 ob_start();
 ?>
 <div class="container-fluid dogs-shortcode">
-	<div class="row">
+	<div class="row page-title-row">
 		<div class="col text-center">
 			<h3>Available Dogs</h3>
 			<h4>last updated ( <?php echo date('m-d-Y');?> )</h4>
 		</div>
 	</div>
-	<div class="row">
+	<div class="row banner-row">
 		<div class="col-12 text-center">
-
-	<div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Available Puppies</h5>
-        <p class="card-text">(dogs under 1 year old)</p>
-      </div>
-    </div>
-		</div>
+	        <h5 class="card-title">Available Puppies</h5>
+	        <p class="card-text">(dogs under 1 year old)</p>
+	    </div>
 	</div>
 
 
-	<div class="row">
+	<div class="row dogs-row">
 		<div class="col">
-			<h4>Name of Dog</h4>
-			<h5>Sex, Age: Months/Years</h5>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col">
-			<div class="card">
+			<div class="card dogs-card">
+				<h4>Name of Dog</h4>
+				<h5>Sex, Age: Months/Years</h5>
 				<img src="https://coastalgsr.org/graphics/thumbnails/22652.jpg" class="card-img-top" alt="dog-image">
 				<div class="card-body">
 					<ul class="list-group list-group-horizontal">
@@ -64,21 +55,17 @@ ob_start();
 		</div>
 	</div>
 
-<div class="row">
+<div class="row banner-row">
 		<div class="col text-center">
 			<h4>Available German Shepherd Dogs</h4>
 		</div>
 	</div>
 
-	<div class="row">
-		<div class="col text-center">
-			<h4>Name of Dog</h4>
-			<h5>Sex, Age: Months/Years</h5>
-		</div>
-	</div>
-	<div class="row">
+	<div class="row dogs-row">
 		<div class="col">
-			<div class="card">
+			<div class="card dogs-card">
+				<h4>Name of Dog</h4>
+				<h5>Sex, Age: Months/Years</h5>
 				<img src="https://coastalgsr.org/graphics/thumbnails/22552.jpg" class="card-img-top" alt="dog-image">
 				<div class="card-body">
 					<ul class="list-group list-group-horizontal">
@@ -91,34 +78,31 @@ ob_start();
 		</div>
 	</div>
 
-<div class="row">
+<!-- Good mockup for loop -->
+<div class="row banner-row">
 		<div class="col text-center">
 			<h4>Available GSD Wannabees!</h4>
 		</div>
 	</div>
 
-
-<div class="row text-center">
-		<div class="col">
+<div class="row dogs-row">
+	<div class="col">
+		<div class="card dogs-card">
 			<h4>Name of Dog</h4>
 			<h5>Sex, Age: Months/Years</h5>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col">
-			<div class="card">
-				<img src="https://coastalgsr.org/graphics/thumbnails/21954.jpg" class="card-img-top" alt="dog-image">
-				<div class="card-body">
-					<ul class="list-group list-group-horizontal">
-						<li class="list-group-item"><a href="#">Sponsor Me</a></li>
-						<li class="list-group-item"><a href="#">I'm Fostered</a></li>
-						<li class="list-group-item"><a href="#">Adopt Me</a></li>
-					</ul>
-				</div>
+			<img src="https://coastalgsr.org/graphics/thumbnails/21954.jpg" class="card-img-top" alt="dog-image">
+			<div class="card-body">
+				<ul class="list-group list-group-horizontal">
+					<li class="list-group-item"><a href="#">Sponsor Me</a></li>
+					<li class="list-group-item"><a href="#">I'm Fostered</a></li>
+					<li class="list-group-item"><a href="#">Adopt Me</a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
+</div>
 
+<!-- END Good mockup for loop -->
 
 </div>
 <?php
